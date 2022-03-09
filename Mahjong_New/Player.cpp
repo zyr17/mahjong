269ebdfff3e217738777e4fd7051c8739e7ce697
agony5757 <chenzhaoyun@iai.ustc.edu.cn>
2022-03-09 17:00:18 +0800

    update

--- Mahjong_New/Player.cpp
+++ Mahjong_New/Player.cpp
@@ -4,6 +4,8 @@
 namespace_mahjong
 using namespace std;
 
+static BaseTile 幺九牌[] = {_1m, _9m, _1p, _9p, _1s, _9s, _1z, _2z, _3z, _4z, _5z, _6z, _7z}; 
+
 Player::Player() 
 { }
 
@@ -265,7 +267,7 @@ vector<SelfAction> Player::get_九种九牌()
 
 	static auto get_九牌 = [](const vector<Tile*> &hand) {		
 		vector<Tile*> 九牌collection;
-		for (int i = BaseTile::_1m; i <= BaseTile::_7z; ++i) {
+		for (auto t : 幺九牌) {
 			auto iter = find_match_tile(hand, BaseTile(i));
 			if (iter != hand.end()) {
 				九牌collection.push_back(*iter);
